ff74addc6616f43f3314095afde6621ff427eedf
Andriy Drobenyuk <drobenyuk.a@gmail.com>
2021-09-13 10:11:48 +0300

    Made basics of Map application

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="app">
+            <header>Header</header>
+            <main>
+                Hello world
+            </main>
+            <footer>
+                Footer
+            </footer>
+        </div>
+    );
+};
+
+export default App;
