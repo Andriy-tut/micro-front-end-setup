8368b23649c2c3642a8f95af6a14c4e6257b99a3
Andriy Drobenyuk <drobenyuk.a@gmail.com>
2021-12-10 15:32:11 +0200

    Added correct initialization of redux devtools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,9 @@
 import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
+
 import session from './session/reducer';
 
 const middleware = applyMiddleware();
-// @ts-ignore
-const devtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
-const composedEnhancers = compose(middleware, devtools)
-
 const rootReducer = combineReducers({ session });
 
-const store = createStore(rootReducer, undefined, composedEnhancers);
-
-export default store;
+export default createStore(rootReducer, undefined, composeWithDevTools(middleware));
