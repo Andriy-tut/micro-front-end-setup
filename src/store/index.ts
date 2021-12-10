503e7f647f6867f859a0f31334b853dbcf445aba
Andriy Drobenyuk <drobenyuk.a@gmail.com>
2021-12-10 16:09:36 +0200

    Fixed issue with redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,8 @@
-import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
+import { combineReducers, createStore, applyMiddleware } from 'redux';
 
 import session from './session/reducer';
 
 const middleware = applyMiddleware();
 const rootReducer = combineReducers({ session });
 
-export default createStore(rootReducer, undefined, composeWithDevTools(middleware));
+export default createStore(rootReducer, undefined, middleware);
