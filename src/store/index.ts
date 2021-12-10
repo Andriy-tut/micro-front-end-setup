ececb2ce78572dca100309f330efbb8579880f71
Andriy Drobenyuk <drobenyuk.a@gmail.com>
2021-12-10 16:58:17 +0200

    Fixed issue with redux dev tools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,14 @@
-import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { compose, combineReducers, createStore, applyMiddleware } from 'redux';
 
 import session from './session/reducer';
 
 const middleware = applyMiddleware();
+const composeEnhancers = (
+    typeof window !== 'undefined' &&
+    process.env.NODE_ENV === 'development' &&
+    // @ts-ignore
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+) || compose;
 const rootReducer = combineReducers({ session });
 
-export default createStore(rootReducer, undefined, middleware);
+export default createStore(rootReducer, undefined, composeEnhancers(middleware));
